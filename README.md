b232967f28543dd09ff0e5d7e3272d5b437c64b7
Jovan Nixon <jmnfire@gmail.com>
2021-03-16 00:12:16 -0600

    Update 2

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 05-Third-Party-APIs-Work-Day-Scheduler
+
+
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com)
+
+[![forthebadge](https://forthebadge.com/images/badges/made-with-crayons.svg)](https://forthebadge.com)
+
+[![forthebadge](https://forthebadge.com/images/badges/winter-is-coming.svg)](https://forthebadge.com)
+
+[![forthebadge](https://forthebadge.com/images/badges/uses-html.svg)](https://forthebadge.com)
+
+[![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,56 @@
+// var today = moment();
+// $('#currentDay').text(today.format('MMM Do, YYYY'));
 
-function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    $('#current-time').text(rightNow);
+var nine = $("#9");
+var ten = $("#10");
+var eleven = $("#11");
+var twelve = $("#12");
+var one = $("#1");
+var two = $("#2");
+var three = $("#3");
+var four = $("#4");
+var five = $("#5");
+var time = moment();
+
+function startSchedule() {
+
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        var planner = localStorage.getItem(id);
+
+        if (planner !== null) {
+            $(this).siblings(".planner").val(planner);
+        }
+    });
 }
 
-setInterval(displayTime, 1000);
+startSchedule();
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var planner = $(this).siblings(".planner").val();
+
+    localStorage.setItem(time, planner);
+});
+
+function colorBlock() {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+colorBlock();
